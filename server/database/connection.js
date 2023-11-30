727f3b09313ebda70fa0a2ae3c950bb0e692ad79
Karunabhutani <91750437+Karunabhutani@users.noreply.github.com>
2023-11-30 19:06:19 +0530

    Add files via upload

---
+++ server/database/connection.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try{
+        // mongodb connection string
+        const con = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true,
+            auth: {
+                user: process.env.karuna0741be21,
+                password: process.env.DwLP0jpuLxfJzpU5,
+              },
+        })
+
+        console.log(`MongoDB connected : ${con.connection.host}`);
+    }catch(err){
+        console.log(err);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB
